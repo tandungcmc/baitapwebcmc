cc570bd3f6033768f68886d6f83b6a64b4c66106
tandungcmc <73791458+tandungcmc@users.noreply.github.com>
2020-11-02 21:24:47 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,28 @@
+<div class="grid_2">
+    <div class="box sidemenu">
+        <div class="block" id="section-menu">
+            <ul class="section menu">
+              	
+                <li><a class="menuitem">Sản phẩm</a>
+                    <ul class="submenu">
+                        <li><a href="productadd.php">Thêm sản phẩm</a> </li>
+                        <li><a href="productlist.php">Danh sách sản phẩm</a> </li>
+                    </ul>
+                </li>
+                <li><a class="menuitem">Phân loại</a>
+                    <ul class="submenu">
+                        <li><a href="brandadd.php">Thêm phân loại</a> </li>
+                        <li><a href="brandlist.php">Danh sách phân loại</a> </li>
+                    </ul>
+                </li>
+                <li><a class="menuitem">Giống chó</a>
+                    <ul class="submenu">
+                        <li><a href="catadd.php">Thêm giống</a> </li>
+                        <li><a href="catlist.php">Danh sách giống</a> </li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</div>
